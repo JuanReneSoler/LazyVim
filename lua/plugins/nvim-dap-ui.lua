3d6d65838aa66e48ed07de2e7f81a08206d936db
Juan René Soler Sirí <juanr.soler_93@hotmail.es>
2025-11-14 23:59:39 -0400

    debugger implementation

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -3,9 +3,12 @@ return {
   dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
   config = function()
     local dap = require("dap")
-    --require("dap").set_log_level("TRACE")
-    --local dapui = require("dapui")
-    --dapui.setup()
+    local dapui = require("dapui")
+
+    --dap.set_log_level("TRACE")
+    dapui.setup()
+
+    --config
     dap.adapters.coreclr = {
       type = "executable",
       command = "/home/jsoler/.local/share/nvim/mason/bin/netcoredbg",
@@ -17,12 +20,40 @@ return {
         name = "launch - netcoredbg",
         request = "launch",
         program = function()
-          return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+          local dlls = vim.fn.systemlist("fd --full-path -e dll bin/Debug")
+
+          if vim.tbl_isempty(dlls) then
+            vim.notify("No .dll found. Build the project first with: dotnet build", vim.log.levels.ERROR)
+            return ""
+          end
+
+          --return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+          return coroutine.create(function(coro)
+            vim.ui.select(dlls, {
+              prompt = "Select .NET DLL to debug:",
+              format_item = function(item)
+                return item
+              end,
+            }, function(choice)
+              coroutine.resume(coro, choice)
+            end)
+          end)
         end,
         cwd = "${workspaceForder}",
         stopAtEntry = false,
         console = "integratedTerminal",
       },
     }
+
+    --keymaps
+    vim.keymap.set("n", "<leader>dt", dapui.toggle, { desc = "Toggle Debugger" })
+    vim.keymap.set("n", "<F5>", function()
+      dapui.open()
+      dap.continue()
+    end)
+    vim.keymap.set("n", "<F9>", dap.toggle_breakpoint)
+    vim.keymap.set("n", "<F10>", dap.step_over)
+    vim.keymap.set("n", "<F11>", dap.step_into)
+    vim.keymap.set("n", "<F12>", dap.step_out)
   end,
 }
